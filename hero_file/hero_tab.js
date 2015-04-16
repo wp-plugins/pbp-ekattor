4c4efa68fe072a4fca268d6968fd1246dd7faa1c
projoktibangla <projoktibangla@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-16 17:34:12 +0000

    bug fixed
    git-svn-id: https://plugins.svn.wordpress.org/pbp-ekattor/tags/1.0@1136371 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hero_file/hero_tab.js
@@ -0,0 +1,16 @@
+(function($)  {
+$(".tab_content").hide();
+$("ul.tabs li:first").addClass("active").show();
+$(".tab_content:first").show();
+$("ul.tabs li").click(function() {
+$("ul.tabs li").removeClass("active");
+$(this).addClass("active");
+$(".tab_content").hide();
+var activeTab = $(this).find("a").attr("href");
+//$(activeTab).fadeIn();
+if ($.browser.msie) {$(activeTab).show();}
+else {$(activeTab).fadeIn();}
+return false;
+});
+})(jQuery);
+
